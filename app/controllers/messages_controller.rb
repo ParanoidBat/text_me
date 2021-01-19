643f76649b355f3553a10fb762a22c8fe1db3b21
Batman <cool_omer@hotmail.co.uk>
2021-01-19 20:43:32 +0500

    chat implement update thru ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,8 @@ class MessagesController < ApplicationController
     message = current_user.messages.build(message_params)
 
     if message.save
-      redirect_to root_path
+      # the second argument is received in chatroom.coffee script
+      ActionCable.server.broadcast "chatroom_channel", msg: message_render(message)
     end
   end
 
@@ -13,5 +14,10 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    # render partial message. the partial requires a message object, send message in that message object
+    render(partial: 'message', locals: {message: message})
   end
 end
