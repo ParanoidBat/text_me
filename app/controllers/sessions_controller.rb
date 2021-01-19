5db986e546cfc65aa142bf451e88199ad7dd5b9f
Batman <cool_omer@hotmail.co.uk>
2021-01-19 17:05:09 +0500

    refactor and authenticcate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,33 @@
 class SessionsController < ApplicationController
   def new
+    if session[:user_id]
+      redirect_to root_path
+    end
   end
   
   def create
+    user = User.find_by(username: params[:session][:username])
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+
+      flash[:success] = "Logged In"
+      redirect_to root_path
+
+    else
+      flash.now[:error] = "Invalid Credentials"
+      render 'new'
+    end
+
   end
 
   def destroy
+    if session[:user_id]
+      session[:user_id] = nil
+      flash[:success] = "Logged Out"
+    end
+
+    redirect_to login_path
   end
 
 end
