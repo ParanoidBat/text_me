0a71620ba8b532cf41fb1711c14377da5e5552bb
Batman <cool_omer@hotmail.co.uk>
2021-01-20 14:44:08 +0500

    moved up the prod line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['https://no-homo-chat-app.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -78,9 +78,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  # allow connection through app
-  config.action_cable.allowed_request_origins = ['https://no-homo-chat-app.herokuapp.com']
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
